efb3ae801bb588d24e2b005e4c2271e16727ccab
Sławomir <ruudn7@wp.pl>
2020-04-07 16:05:17 +0200

    init react project

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText] = useState('Hello world!')
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{outputText}</Text>
+      <Button
+        onPress={() => setOutputText('Hello world! Already text update')}
+        title="Learn More"/>
     </View>
   );
 }
